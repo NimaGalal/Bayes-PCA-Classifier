7406d36d9766393ddd84bd9dd1f368846b5e4b25
Nima <nimaagalal@gmail.com>
2026-03-11 01:00:39 +0200

    Refactor PCA evaluation with baseline accuracy

--- Experiment_B.py
+++ Experiment_B.py
@@ -1,28 +1,48 @@
 # Experiment B
+
 from sklearn.metrics import confusion_matrix , accuracy_score , classification_report
 from naivebayesclassifier import NumericalNaiveBayes
 from pca import PCA_implementation
 from dataloading import dataLoadingAndExtractingNumericalFeatures
 
-
 #Passing the PC's to the Naiive bayes Classifier
 #Evaluation
 
 X_train, X_test, Y_train, Y_test = dataLoadingAndExtractingNumericalFeatures()
-def NB_PCA (k) :
+
+def get_basline (X_train, X_test, Y_train, Y_test) :
+
+    nb1 =  NumericalNaiveBayes()
+    cm1 , accs1 , cr1 = nb1.evaluate (X_train , Y_train ,X_test , Y_test)
+    print ('Results before applying PCA')
+    print (cm1)
+    print (accs1)
+    print (cr1)
+    return accs1             
+
+
+def NB_PCA (k,baseline_accuracy) :
+    
     Final_result_XTrain , Final_result_XTest = PCA_implementation(X_train, X_test, k)
-    nb = NumericalNaiveBayes()
-    cm , accs , cr = nb.evaluate (Final_result_XTrain , Y_train ,
+    nb2 = NumericalNaiveBayes()
+    cm2 , accs2 , cr2 = nb2.evaluate (Final_result_XTrain , Y_train ,
                               Final_result_XTest, Y_test)
-    print (cm)
-    print (accs)
-    print (cr)
+    print ('Results after applying PCA')
+    print (cm2)
+    print (accs2)
+    print (cr2)
 
 #Comparison with baseline 
-
-
+    print (f"basline accuracy : {baseline_accuracy:.2%}")
+    print (f"PCA accuracy : k= {k} {accs2:.2%}")
+    if baseline_accuracy > accs2  :
+       print ('basline is more accurate than PCA ')
+    else :
+      print ('PCA is more accurate than baseline')
+    
 
 #k Experiments 
+baseline_accuracy = get_baseline(X_train, X_test, Y_train, Y_test)
 k_values = [10,20,30,40,50]
 for k in k_values :
-    NB_PCA (k)
+    NB_PCA (k,baseline_accuracy)
